724166e8c7de873c458f6289593c91bb519f79a8
pascal.lauener <pascal@lauener.biz>
2022-05-15 14:45:06 +0200

    update link

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ function HomePage() {
         <p>Passcal Lauener</p>
         <p>Flossenmatt 7c</p>
         <p>6045 Meggen</p>
-        <a href='./api/card'>link</a>
+        <a href='./api/card'>save me on your phone</a>
     </Layout>
   );
 }
