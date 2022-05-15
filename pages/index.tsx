c01d1d33062777cc40db40d7115b276d21861c47
pascal.lauener <pascal@lauener.biz>
2022-05-15 17:34:31 +0200

    update style

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,33 @@ import { Button } from '@nextui-org/react';
 import { Card } from '@nextui-org/react';
 import { Link } from '@nextui-org/react';
 import { Image } from '@nextui-org/react';
+import { User } from '@nextui-org/react';
+import { Avatar } from '@nextui-org/react';
+
 
 
 function HomePage() {
   return (
 
 <Layout>
-<Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
-      <Image
-        showSkeleton
-        width={320}
-        height={180}  
-        src="https://media-exp1.licdn.com/dms/image/C4E03AQE8WYAr7wCsCg/profile-displayphoto-shrink_800_800/0/1651918608978?e=1658361600&v=beta&t=BjVxxLTz5u8Y_-79OAnPSFVR6h9835l_gZlxWOKcZ1o"
-        alt="Default Image"
-        objectFit="cover"
-      />
+  <Card bordered shadow={true} hoverable css={{ mw: "400px" }}>
+    <Avatar
+      zoomed
+      src="https://media-exp1.licdn.com/dms/image/C4E03AQE8WYAr7wCsCg/profile-displayphoto-shrink_800_800/0/1651918608978?e=1658361600&v=beta&t=BjVxxLTz5u8Y_-79OAnPSFVR6h9835l_gZlxWOKcZ1o"
+      css={{ size: "$40" }}
+    />
       </Card>
-      <Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
+      <Card bordered shadow={true} hoverable css={{ mw: "400px" }}>
         <h1>{config.firstName} {config.lastName}</h1>
         <p>Director Software Development</p>
       </Card>
-      <Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
+      <Card bordered shadow={true} hoverable css={{ mw: "400px" }}>
       <p>{config.address}</p>
         <p>{config.zipCode} {config.location}</p>
         <p>{config.phone}</p>
         <p>{config.mail}</p>
       </Card>
-      <Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
+      <Card bordered shadow={true} hoverable css={{ mw: "400px" }}>
         <Link href='./api/card'>save me as contact</Link>
       </Card>
     </Layout>
