683dd4dcefede2ba75eefe16260fb352e7f069c9
laupas <37934385+laupas@users.noreply.github.com>
2022-05-21 10:16:37 +0200

    Update card.tsx

--- pages/api/card.tsx
+++ pages/api/card.tsx
@@ -16,6 +16,7 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     .addEmail(config.mail)
     .addPhoneNumber(config.phone, 'WORK');
 //    .addAddress(null, null, 'street', 'worktown', null, 'workpostcode', 'Belgium')
+      .addUrl(fileName)
 //    .addURL('http://www.callista.ch');
   
     res.setHeader('Content-Type', 'text/vcard; name="{fileName}.vcf"');
