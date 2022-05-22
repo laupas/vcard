9f85dbdaec9223331d3440eac6e1878017d091c9
pascal.lauener <pascal@lauener.biz>
2022-05-22 11:46:30 +0200

    fix image

--- pages/api/card.tsx
+++ pages/api/card.tsx
@@ -1,15 +1,21 @@
 import VCard from 'vcard-creator'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import config from 'components/config';
+import fs from 'fs'
+import path from 'path'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
  
+  const dir = path.resolve('./public');
+  const image = fs.readFileSync(dir + '/picture.jpeg', { encoding: 'base64', flag: 'r' })
+
   const myVCard = new VCard()
   var fileName = new Date().getTime();
+  
   myVCard
     // Add personal data
     .addName(config.firstName, config.lastName, "", "", "")
-    .addPhotoURL("https://media-exp1.licdn.com/dms/image/C4E03AQE8WYAr7wCsCg/profile-displayphoto-shrink_800_800/0/1651918608978?e=1658361600&v=beta&t=BjVxxLTz5u8Y_-79OAnPSFVR6h9835l_gZlxWOKcZ1o")
+    .addPhoto(image, 'JPEG')
 
     // Add work data  
     .addCompany(config.companyName)
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ function HomePage() {
       <Card bordered shadow={true} hoverable css={{ mw: "400px" }}>
         <Avatar
           zoomed
-          src="https://media-exp1.licdn.com/dms/image/C4E03AQE8WYAr7wCsCg/profile-displayphoto-shrink_800_800/0/1651918608978?e=1658361600&v=beta&t=BjVxxLTz5u8Y_-79OAnPSFVR6h9835l_gZlxWOKcZ1o"
+          src="./picture.jpeg"
           css={{ size: "$20" }}
         />
       </Card>
